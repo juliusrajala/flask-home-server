a4e4ca8f89cde1deeb2cb59642371180786a014b
Julius Rajala <juliusrajala@gmail.com>
2019-11-06 18:46:46 +0200

    Added host to open development-path.

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from project import app
 if __name__ == "__main__":
     port = int(os.environ.get("APP_PORT", 5000))
     print(f' *** Starting Flask Server in port {port} *** ')
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
