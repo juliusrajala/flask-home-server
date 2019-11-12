e23018c72fc016a680c16e996be1244498252188
Julius Rajala <juliusrajala@gmail.com>
2019-11-12 12:38:07 +0200

    Built custom auth-flow against authorization code flow.

--- project/routes/spotify.py
+++ project/routes/spotify.py
@@ -2,10 +2,10 @@ from flask import request, jsonify, Response, redirect
 from project.routes import routes
 from project.services.spotify import init_search_service, PlayerService, build_auth_uri
 import functools
-import json
 
 SpotifyService = PlayerService(init_search_service())
-spotify_client = SpotifyService.client
+spotify_client = SpotifyService.get_client
+
 
 def map_response_item(item):
     return {
@@ -13,32 +13,46 @@ def map_response_item(item):
         'artist': ', '.join(list(map(
             lambda x: x['name'],
             item['artists']
-            ))),
+        ))),
         'id': item['id']
     }
+
 
 @routes.route('/v1/spotify/search')
 def search():
     search_string = request.args.get('query', '')
     if search_string == '':
         return 'Empty search string'
-    result = spotify_client.search(search_string, type='track', limit=20)
+    result = spotify_client().search(search_string, type='track', limit=20)
     return jsonify(list(map(lambda a: map_response_item(a), result['tracks']['items'])))
+
 
 @routes.route('/v1/spotify/now_playing')
 def now_playing():
     if not SpotifyService.is_player_client:
         return 'Client not configured for user'
     print('Spotify accessToken is', SpotifyService.access_token)
-    response = spotify_client.current_user()
+    response = spotify_client().current_user()
     return response
+
 
 @routes.route('/v1/spotify/auth')
 def authenticate():
     if SpotifyService.registration_secret == '':
         generated_secret = 'Secret_Token'
-        SpotifyService.set_registered(generated_secret) # Replace secret token with generated one.
+        # Replace secret token with generated one.
+        SpotifyService.set_registered(generated_secret)
         return redirect(build_auth_uri(generated_secret), code=302)
-    auth_code = request.args.get('code')
-    spotify_client = SpotifyService.inject_token(auth_code)
-    return f'Authentication done with code {auth_code}'
+    access_code = request.args.get('code')
+
+    if SpotifyService.access_token:
+        print(f'Access token already found {access_code}')
+        return jsonify(spotify_client().current_user())
+
+    received_secret = request.args.get('state')
+
+    if received_secret == SpotifyService.registration_secret and access_code:
+        SpotifyService.validate_tokens(access_code)
+        return f'Updating access tokens with {access_code}'
+
+    raise Exception('Something went wrong')
--- project/services/spotify.py
+++ project/services/spotify.py
@@ -1,17 +1,18 @@
 import os
 import spotipy
-import spotipy.oauth2 as SPOauth
-import webbrowser
-from spotipy.util import prompt_for_user_token
-from enum import Enum
+import spotipy.oauth2 as spoAuth2
+import json
+import requests
 
 client_id = str(os.environ.get('SPOTIFY_CLIENT_ID', ''))
 client_secret = str(os.environ.get('SPOTIFY_CLIENT_SECRET', ''))
 spotify_username = str(os.environ.get('SPOTIFY_USERNAME', ''))
 redirect_uri = str(os.environ.get('SPOTIFY_REDIRECT_URL', ''))
+token_endpoint = 'https://accounts.spotify.com/api/token'
+
 
 def build_auth_uri(secret):
-    client_scope = "user-modify-playback-state user-read-currently-playing user-read-playback-state"
+    client_scope = "user-modify-playback-state user-read-currently-playing user-read-playback-state user-read-private"
     return ''.join([
         'https://accounts.spotify.com/authorize?client_id=',
         client_id,
@@ -20,18 +21,24 @@ def build_auth_uri(secret):
         '&scope=',
         client_scope,
         '&state=',
-        secret
+        secret,
+        '&show_dialog=true'
     ])
+
 
 def init_search_service():
     if client_id == '' or client_secret == '':
         raise Exception('Invalid client_id or client_secret')
-    credentials_manager = SPOauth.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    spotipy_client = spotipy.Spotify(client_credentials_manager=credentials_manager)
+    credentials_manager = spoAuth2.SpotifyClientCredentials(
+        client_id=client_id, client_secret=client_secret)
+    spotipy_client = spotipy.Spotify(
+        client_credentials_manager=credentials_manager)
     return spotipy_client
+
 
 class PlayerService:
     """Using the spotify authorization code flow. """
+
     def __init__(self, client):
         self.registration_secret = ''
         self.is_player_client = False
@@ -39,14 +46,33 @@ class PlayerService:
         self.access_token = ''
         self.client = client
 
+    def _post(self, data):
+        return requests.post(token_endpoint, data)
+
+    def get_client(self):
+        return self.client
+
     def set_registered(self, secret):
         self.registration_secret = secret
 
-    def inject_token(self, auth_code):
-        self.access_token = auth_code
-        if auth_code == '':
-            raise Exception('Bad auth_code')
+    def validate_tokens(self, access_code):
+        response = self._post({
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'authorization_code',
+            'code': access_code,
+            'redirect_uri': redirect_uri
+        })
+        response_data = json.loads(response.text)
+        if response_data['access_token']:
+            self.inject_token(response_data['access_token'])
+        for i in response_data:
+            print("key: ", i, "val: ", response_data[i])
 
-        self.client = spotipy.Spotify(auth=auth_code)
+    def inject_token(self, access_token):
+        self.access_token = access_token
+        if access_token == '':
+            raise Exception('Access token was called with an empty string')
+
+        self.client = spotipy.Spotify(auth=access_token)
         self.is_player_client = True
-        return self.client
